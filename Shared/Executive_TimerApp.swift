8695f72246635a1fae3fabd0174ef8017dac98ab
czp356 <czp356@gmail.com>
2022-08-30 15:12:38 +0800

    Initial Commit

---
+++ Shared/Executive_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TimerApp.swift
+//  Shared
+//
+//  Created by 常志平 on 30/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
