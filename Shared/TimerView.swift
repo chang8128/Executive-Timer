0059e41a1a902a2ad39d1b6ea2811375f96300ad
czp356 <czp356@gmail.com>
2022-09-02 14:59:21 +0800

    revise for some memo.

--- Shared/TimerView.swift
+++ Shared/TimerView.swift
@@ -21,25 +21,31 @@ struct TimerView: View {
     
     var body: some View {
         ZStack {
-            Circle().stroke(Color.gray.opacity(0.2), style: strokeStyle)
-            Circle().trim(from: 0, to: 1 - (defaultTime - countdownTime) / defaultTime)
-                .stroke(countdownColor, style: strokeStyle)
-                .rotationEffect(.degrees(-90))
-                .animation(.easeInOut(duration: 0.2), value: countdownTime)
-            HStack(spacing: 25) {
-                Label("", systemImage: timerRunning ? "pause.rectangle.fill": "play.rectangle.fill")
-                    .foregroundColor(.black).font(.title)
+            Circle()
+                .stroke(Color.gray.opacity(0.2), style: strokeStyle)
+            Circle()
+                .trim(from: 0, to: 1 - (defaultTime - countdownTime) / defaultTime)       // trim the shape by a fractional amount, based on its representation as a path.
+                .stroke(countdownColor, style: strokeStyle)                               // .stroke sets the color of the outline of the shape
+                .rotationEffect(.degrees(-90))                     // .rotationEffect rotates the element by the specified amount, begin at 12 o'clock point, 0 is at the 15 minutes point.
+                .animation(.easeInOut(duration: 0.2), value: countdownTime)               // .animation sets the speed at which any animation takes place in the view.
+            HStack(spacing: 70) {
+                Label("", systemImage: timerRunning ? "pause.rectangle.fill": "play.rectangle.fill")    // icon click to start
+                    .foregroundColor(.green).font(.title)
                     .onTapGesture(perform: { timerRunning.toggle() })
-                Text("\(Int(countdownTime))")
-                Label("", systemImage: "gobackward")
+                Text("\(Int(countdownTime))")                                                          // the init time of timer
+                Label("", systemImage: "gobackward.60")                                                // icon of the restart
                     .foregroundColor(.red)
                     .font(.title)
                     .onTapGesture(perform: {
                         timerRunning = false
-                        countdownTime = defaultTime
+                        countdownTime = defaultTime    // When the third label with the reset icon, is tapped, the timerRunning Boolean is set to false, and the countdownTime is set back to the defaultTime
                     })
             }
-        }.frame(width: 300, height: 300)
+        }.frame(width: 300, height: 300)               // We've framed the ZStack to a nice 300*300 square,
+            
+            /* .onReceive is an instance method available on views that lets you specify a publisher to respond to, and a method to run when that publisher emits data.
+             The perform parameter specifies the method to be run when the specified publisher (in this case, timer) emit something.
+             */
             .onReceive(timer, perform: {_ in
                 guard timerRunning else { return }
                 if countdownTime > 0 {
